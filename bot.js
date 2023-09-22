cccbcfae1e9d706c78a544ae6cd9841ab833d8b7
Mejoramos Juntos <mejoramosjuntos@proton.me>
2023-09-22 00:16:23 -0300

    Cambié la frecuencia de los mensajes

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ telegraf.start((ctx) => {
 let timer = null;
 
 timer = setInterval(() => {
-    if((new Date().getHours() % 3) === 0) {
+    if((new Date().getMinutes() % 3) === 0) {
         telegram.sendMessage('@MJ_prueba1', 'hola');    
     }
-}, 3600000)  
+}, 20000)  
 
 telegraf.launch()
