c6120ccb9729b62e9155e3985608e0d2e8a39af1
Mejoramos Juntos <mejoramosjuntos@proton.me>
2023-09-21 22:58:05 -0300

    Primer commit

---
+++ bot.js
@@ -0,0 +1,19 @@
+import { Telegraf } from 'telegraf';
+import { Telegram } from 'telegraf';
+
+const telegraf = new Telegraf('6153297281:AAGJBkOMmQgzvuzf42oz_ZhgeasX_UoybAw')
+const telegram = new Telegram('6153297281:AAGJBkOMmQgzvuzf42oz_ZhgeasX_UoybAw')
+
+telegraf.start((ctx) => {
+  ctx.reply('Welcome');
+})
+
+let timer = null;
+
+timer = setInterval(() => {
+    if((new Date().getHours() % 3) === 0) {
+        telegram.sendMessage('@MJ_prueba1', 'hola');    
+    }
+}, 3600000)  
+
+telegraf.launch()
